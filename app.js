c68a8f548736c1fb72be1c39310c7763ad0233e1
ahmedali081234 <ahmed.ali.081234@gmail.com>
2025-10-29 12:27:41 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Question Number 1
+
+alert('Welcome to my website')
+
+// Question Number 2
+
+alert('Error! Please enter a valid passward')
+
+// Question Number 3
+
+alert('Welcome to JS Land... \n Happy coding!')
+
+
+// Question Number 4
+
+alert('Welcome to JS Land... ')
+alert('Happy Coding! \n Prevent this page from creating additional dialogs. ')
+
+// Question Number 5
+console.log("Hello... I can run JS through my web browser's console");
---
+++ app1.js
@@ -0,0 +1,2 @@
+// Question #6
+alert('Hello visitor!')
